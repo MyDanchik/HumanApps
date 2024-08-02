c009a029f0ad64cfe68dee62f4b3f9581c7b76aa
Daniil <idanik.mega@gmail.com>
2024-08-02 12:51:04 +0300

    Initial Commit

---
+++ HumanApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HumanApps
+//
+//  Created by Daniil Yushkevich on 2.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
