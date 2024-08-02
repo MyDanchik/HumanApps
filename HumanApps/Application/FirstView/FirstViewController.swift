52aa509b0d06cd3c48159b9751546bdd2b098b74
Daniil <idanik.mega@gmail.com>
2024-08-02 13:37:57 +0300

    TabBar

---
+++ HumanApps/Application/FirstView/FirstViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class FirstViewController: UIViewController {
+    var viewModel: FirstViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
